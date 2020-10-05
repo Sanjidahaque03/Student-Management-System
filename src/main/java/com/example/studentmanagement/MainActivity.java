ccf4eb99bc9ea92fec68b7bfd0512d17a5aca304
Sanjida Eva <62129264+Sanjidahaque03@users.noreply.github.com>
2020-10-05 09:08:04 +0600

    System to add student details
    Student managemant system with one of its features i.e. to adding student details.

--- src/main/java/com/example/studentmanagement/MainActivity.java
+++ src/main/java/com/example/studentmanagement/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.studentmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText name,email,cgpa;
+    private EditText name,id,cgpa;
     private Button button;
     private TextView textView;
 
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         name =  (EditText)  findViewById(R.id.name_id);
-        email =  (EditText)  findViewById(R.id.email_id);
+        id =  (EditText)  findViewById(R.id.id_id);
         cgpa =  (EditText)  findViewById(R.id.cgpa_id);
         textView = (TextView) findViewById(R.id.textView_id);
 
@@ -29,26 +30,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button.setOnClickListener(this);
 
 
+        
     }
 
     @Override
     public void onClick(View v) {
-
         String name1 = name.getText().toString();
-        String email1 = email.getText().toString();
+        String id1 = id.getText().toString();
         String cgpa1 = cgpa.getText().toString();
+
+        TableLayout table = (TableLayout) findViewById(R.id.tb);
+        TableRow row = new TableRow(this);
+        TextView tv = new TextView(this);
+
+
+
         if (v.getId()==R.id.button_id){
-            textView.setText("Name: "+name1+"\nEmail: "+email1+"\nCGPA: "+cgpa1);
-            clearText();
+
+            textView.setText("Name: "+name1+"\nEmail: "+id1+"\nCGPA: "+cgpa1);
+            
+            tv.setText(name1);
+            tv.setText(id1);
+            tv.setText(cgpa1);
+
+            row.addView(tv);
+            table.addView(row);
         }
 
 
+
+
+
+        clearText();
     }
 
     public void clearText()
     {
         name.setText("");
-        email.setText("");
+        id.setText("");
         cgpa.setText("");
         name.requestFocus();
     }
